a691042b1a4e010aad80ed3074cfd38d379f81ef
martynow173 <e.martinow@yandex.ru>
2017-04-13 10:55:43 +0400

    second try

--- send.php
+++ send.php
@@ -1,19 +1,18 @@
-<?php
-    $name = $_POST['name']; // Вытаскиваем имя в переменную
-    $number = $_POST['number']; // Вытаскиваем почту в переменную
-    $special = $_POST['special'];
-    $time = $_POST['time'];
-    $address = $_POST['address'];
-    $message = "Клиент $name (телефонный номер $number) произвел онлайн-запись на прием. Информация: $special. Желает записаться на прием по адрессу $address в назначенное время: $time.";
-    $to = "e.martinow@yandex.ru"; // Задаем получателя письма
-    $from = "noreply-martynow@ulybka.ru"; // Отправитель
-    $subject = "Запись на прием"; // Тема
-    $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; //для формирвоания заголовка
-    if (mail($to, $subject, $message, $headers)) {
-        echo "<p>Сообщение успешно отправлено! Ожидайте, с вами обязательно свяжуться!</p>";
-    }
-    else {
-        echo "<p>Ошибка отправления</p>"; 
-    }
-
+<?php 
+$name = $_POST['name']; // Вытаскиваем имя в переменную 
+$number = $_POST['number']; // Вытаскиваем почту в переменную 
+$special = $_POST['special']; 
+$time = $_POST['time']; 
+$address = $_POST['address']; 
+$message = "Клиент $name (телефонный номер $number) произвел онлайн-запись на прием. Информация: $special. Желает записаться на прием по адресу $address в назначенное время: $time."; 
+$to = "e.martinow@yandex.ru"; // Задаем получателя письма 
+$from = "noreply-martynow@ulybka.ru"; // Отправитель 
+$subject = "Запись на прием"; // Тема 
+$headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; //для формирвоания заголовка 
+if (mail($to, $subject, $message, $headers)) { 
+echo "<p>Сообщение успешно отправлено! Ожидайте, с вами обязательно свяжутся!</p><br><a href='index.html'><input type='button' value='Вернуться на главную'</a>"; 
+} 
+else { 
+echo "<p>Ошибка отправления</p><br><a href='index.html'><input type='button' value='Вернуться на главную'</a>"; 
+} 
 ?>
